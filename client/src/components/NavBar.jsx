5cd5ab59f97a401fc5c72d944d81c0eb24e9ffa4
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-17 22:57:45 -0900

    add a link on the NavBar component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/register'>SignUp</Link>
+      <Link to='/login'>Login</Link>
+    </nav>
   )
 }
 
