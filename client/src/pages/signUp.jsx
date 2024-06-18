69742e55a2987ee9f072a9a2a45e5f42f92ba256
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-17 23:03:42 -0900

    working a simple signup page

--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>Sign up</div>
+    <div>
+      <form action="post">
+        <label htmlFor="name">Name</label>
+        <input type="text" placeholder='Enter ur name...'/>
+        <label htmlFor="email">Email</label>
+        <input type="email" placeholder='Enter ur email...'/>
+        <label htmlFor="password">Name</label>
+        <input type="password" placeholder='Enter ur password...'/>
+      </form>
+    </div>
   )
 }
