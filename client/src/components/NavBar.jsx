9fcbea8c2d4df053071d7a7b9f92b1c5149f08d9
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-17 22:09:38 -0900

    asign the jsx component code and import the browserRouter component in main.jsx file

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function NavBar() {
+function NavBar() {
   return (
     <div>NavBar</div>
   )
 }
+
+export default NavBar
---
+++ client/src/pages/logIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function logIn() {
+  return (
+    <div>logIn</div>
+  )
+}
+
+export default logIn
