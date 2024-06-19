b9f7aa1814cc261bdb44f01fe888b7954a9614cb
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-18 22:59:05 -0900

    edit signup and login pages

--- client/src/pages/logIn.jsx
+++ client/src/pages/logIn.jsx
@@ -1,27 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function LogIn() {
-
   const [data, setData] = useState({
-    email:'',
-    password:'',
-  })
+    email: "",
+    password: "",
+  });
 
   const loginUser = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   return (
     <div>
       <form onSubmit={loginUser}>
-      <label htmlFor="email">Email</label>
-        <input type="email" placeholder='Enter ur email...' value={data.email} 
-        onChange={(e) => setData({...data, email: e.target.value})}/>
-        <label htmlFor="password">Name</label>
-        <input type="password" placeholder='Enter ur password...' value={data.password} 
-        onChange={(e) => setData({...data, password: e.target.value})}/>
-        <button type='submit'>submit</button>
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="Enter ur email..."
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label>Name</label>
+        <input
+          type="password"
+          placeholder="Enter ur password..."
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">submit</button>
       </form>
     </div>
-  )
+  );
 }
--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -1,31 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function SignUp() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  const[data, setData] = useState({
-    name:'',
-    email:'',
-    password:'',
-  })
-
-  const registerUser = (e) =>{
-    e.preventDefault()
-  }
+  const registerUser = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
       <form onSubmit={registerUser}>
-        <label htmlFor="name">Name</label>
-        <input type="text" placeholder='Enter ur name...' value={data.name} 
-        onChange={(e) => setData({...data, name: e.target.value})}/>
-        <label htmlFor="email">Email</label>
-        <input type="email" placeholder='Enter ur email...' value={data.email} 
-        onChange={(e) => setData({...data, email: e.target.value})}/>
-        <label htmlFor="password">Name</label>
-        <input type="password" placeholder='Enter ur password...' value={data.password} 
-        onChange={(e) => setData({...data, password: e.target.value})}/>
-        <button type='submit'>submit</button>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Enter ur name..."
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="Enter ur email..."
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label>Name</label>
+        <input
+          type="password"
+          placeholder="Enter ur password..."
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">submit</button>
       </form>
     </div>
-  )
+  );
 }
