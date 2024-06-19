1ec955a45ee64dc524b1e6b7381f4a2b83ae0dea
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-18 22:09:55 -0900

    add teh event and usestate on signup page

--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function SignUp() {
+
+  const[data, setData] = useState({
+    name:'',
+    email:'',
+    password:'',
+  })
 
   const registerUser = (e) =>{
     e.preventDefault()
@@ -10,11 +16,14 @@ export default function SignUp() {
     <div>
       <form onSubmit={registerUser}>
         <label htmlFor="name">Name</label>
-        <input type="text" placeholder='Enter ur name...'/>
+        <input type="text" placeholder='Enter ur name...' value={data.name} 
+        onChange={(e) => setData({...data, name: e.target.value})}/>
         <label htmlFor="email">Email</label>
-        <input type="email" placeholder='Enter ur email...'/>
+        <input type="email" placeholder='Enter ur email...' value={data.email} 
+        onChange={(e) => setData({...data, email: e.target.value})}/>
         <label htmlFor="password">Name</label>
-        <input type="password" placeholder='Enter ur password...'/>
+        <input type="password" placeholder='Enter ur password...' value={data.password} 
+        onChange={(e) => setData({...data, password: e.target.value})}/>
         <button type='submit'>submit</button>
       </form>
     </div>
