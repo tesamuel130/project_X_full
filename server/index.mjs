1cea031b0e99b182a31147206a076150e7296287
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-19 01:28:50 -0900

    install mongoos pkg and import on index.js file

--- server/index.mjs
+++ server/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 import router from "./routes/router.mjs";
 
 dotenv.config();
