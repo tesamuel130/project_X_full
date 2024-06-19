3060f340fb535f709a6d47d9ed5d566e4911e05c
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-18 21:50:47 -0900

    update the signup form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from '../src/components/NavBar'
 import Home from './pages/Home'
-import SignUp from './pages/signUp'
+import SignUp from './pages/SignUp'
 import LogIn from './pages/LogIn'
 
 function App() {
--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
 export default function SignUp() {
+
+  const registerUser = (e) =>{
+    e.preventDefault()
+  }
+
   return (
     <div>
-      <form action="post">
+      <form onSubmit={registerUser}>
         <label htmlFor="name">Name</label>
         <input type="text" placeholder='Enter ur name...'/>
         <label htmlFor="email">Email</label>
         <input type="email" placeholder='Enter ur email...'/>
         <label htmlFor="password">Name</label>
         <input type="password" placeholder='Enter ur password...'/>
+        <button type='submit'>submit</button>
       </form>
     </div>
   )
