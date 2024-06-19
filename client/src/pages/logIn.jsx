8b78eac94075a91d53378f46056abb1c1836f88c
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-18 22:13:11 -0900

    add the event and import the use state on login page

--- client/src/pages/logIn.jsx
+++ client/src/pages/logIn.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function LogIn() {
+
+  const [data, setData] = useState({
+    email:'',
+    password:'',
+  })
 
   const loginUser = (e) => {
     e.preventDefault()
@@ -10,9 +15,11 @@ export default function LogIn() {
     <div>
       <form onSubmit={loginUser}>
       <label htmlFor="email">Email</label>
-        <input type="email" placeholder='Enter ur email...'/>
+        <input type="email" placeholder='Enter ur email...' value={data.email} 
+        onChange={(e) => setData({...data, email: e.target.value})}/>
         <label htmlFor="password">Name</label>
-        <input type="password" placeholder='Enter ur password...'/>
+        <input type="password" placeholder='Enter ur password...' value={data.password} 
+        onChange={(e) => setData({...data, password: e.target.value})}/>
         <button type='submit'>submit</button>
       </form>
     </div>
