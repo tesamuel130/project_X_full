f0939c55ee3346268724daba995b34b461298347
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-23 10:34:29 -0900

    remove the core pkg from the index file

--- server/index.mjs
+++ server/index.mjs
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import { dbConnection } from "./models/DbConnection.mjs";
 import cookieParser from "cookie-parser";
 
