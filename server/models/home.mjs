12a79c726c8c0950223721cadf826c22cb546132
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-23 10:54:14 -0900

    edit some code inthe backend

--- server/models/home.mjs
+++ server/models/home.mjs
@@ -1,1 +1,20 @@
 import mongoose from "mongoose";
+
+export const homeDataFeatch = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  gener: {
+    type: String,
+    required: true,
+  },
+});
