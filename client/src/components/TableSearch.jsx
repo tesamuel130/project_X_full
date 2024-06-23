8fe8a5ad8c48df93a97dfd5cb6e56132751feb0f
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-22 20:59:52 -0900

    edit a table component

--- client/src/components/TableSearch.jsx
+++ client/src/components/TableSearch.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { users } from "../tryusers";
 
 export default function TableSearch() {
+  const [query, setQuery] = useState("");
   return (
-    <table>
-      <tbody>
-        <tr></tr>
-      </tbody>
-    </table>
+    <div className="app">
+      <input
+        type="text"
+        placeholder="search"
+        className="trysearch"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <table>
+        <tbody>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>gender</th>
+          </tr>
+
+          {users.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
