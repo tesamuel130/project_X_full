36809a24a8791a2304247a53a52dacfbcc87d78f
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-23 21:40:10 -0900

    working on the payauth page

--- client/src/pages/PaymentAuth.jsx
+++ client/src/pages/PaymentAuth.jsx
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 
 //import fontawsome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleDot,
-  faDollarSign,
-  faPhone,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 
 //import video img
 import vidImg0 from "../assets/images/chanel-2.png";
@@ -20,8 +16,29 @@ export default function () {
           <div className="col-md-6"></div>
           <div className="col-md-6 pay-form-cont">
             <form action="post" className="pay-form">
-              <label htmlFor="name">Amount</label>
-              <input type="text" placeholder="999" />
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                placeholder="please enter your account name..."
+              />
+              <label htmlFor="name">Account</label>
+              <input type="text" placeholder="pleace enter your account No." />
+              <div className="amount-input">
+                <select name="paymentOption" id="">
+                  <option value="selectPaymentCurrency">
+                    select Payment Currency
+                  </option>
+                  <option value="ETB">ETB</option>
+                  <option value="USD">USD</option>
+                  <option value="CAD">CAD</option>
+                  <option value="BTC">BTC</option>
+                </select>
+                <label htmlFor="chatMin">Input Chat/Min</label>
+                <input type="text" placeholder="min $5/M" />
+                <label htmlFor="name">Total Amount</label>
+                <input type="text" placeholder="999" />
+              </div>
+
               <div className="payment-method">
                 <select name="payment" id="">
                   <option value="">Select Payment Method</option>
@@ -30,11 +47,17 @@ export default function () {
                   <option value="COOP">COOP</option>
                   <option value="telebirr">TeleBirr</option>
                   <option value="AWASH">AWASH</option>
+                  <option value="BINANCE">BINANCE</option>
+                  <option value="paypal">PAYPAL</option>
                 </select>
                 <div className="account-numb">
                   <input type="text" placeholder="1000*****12" />
+                  <FontAwesomeIcon className="copy-account" icon={faCopy} />
                 </div>
               </div>
+              <label htmlFor="img">Input Recite</label>
+              <input type="file" />
+              <button type="submit">Submit</button>
             </form>
           </div>
         </div>
