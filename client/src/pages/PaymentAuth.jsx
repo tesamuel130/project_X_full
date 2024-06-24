9b2fcf75c5ba6ee58e7e7935d35852c0b6d89261
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-06-23 21:23:41 -0900

    working on the paymenth auth page

--- client/src/pages/PaymentAuth.jsx
+++ client/src/pages/PaymentAuth.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+//import fontawsome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleDot,
+  faDollarSign,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 //import video img
 import vidImg0 from "../assets/images/chanel-2.png";
@@ -9,7 +18,25 @@ export default function () {
       <div className="container">
         <div className="row">
           <div className="col-md-6"></div>
-          <div className="col-md-6 justify-content-center">div</div>
+          <div className="col-md-6 pay-form-cont">
+            <form action="post" className="pay-form">
+              <label htmlFor="name">Amount</label>
+              <input type="text" placeholder="999" />
+              <div className="payment-method">
+                <select name="payment" id="">
+                  <option value="">Select Payment Method</option>
+                  <option value="CBE">CBE</option>
+                  <option value="BOA">BOA</option>
+                  <option value="COOP">COOP</option>
+                  <option value="telebirr">TeleBirr</option>
+                  <option value="AWASH">AWASH</option>
+                </select>
+                <div className="account-numb">
+                  <input type="text" placeholder="1000*****12" />
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
