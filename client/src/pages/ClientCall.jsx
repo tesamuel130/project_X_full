c554d3fc8e3233958fb7651bb0bd399ded89fc2b
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-01 08:13:46 -0900

    working on clientCall stracture and style

--- client/src/pages/ClientCall.jsx
+++ client/src/pages/ClientCall.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import { TextField } from "@material-ui/core";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import PhoneIcon from "@material-ui/icons/Phone";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import peer from "simple-peer";
+import io from "socket.io-client";
 
 export default function ClientCall() {
-  return <div>ClientCall</div>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
