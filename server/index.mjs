d0e3336f256ef4801a1603cc202917cea709524f
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-03 06:57:07 -0900

    fix the error for the browser side by using npm install global  pkg

--- server/index.mjs
+++ server/index.mjs
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import { dbConnection } from "./config/DbConnection.mjs";
 import cookieParser from "cookie-parser";
 
+//fix the browser error
+import global from "global";
+window.global = global;
+
 //import pkgs used for the video call
 import http from "http";
 import { Server as SocketIoServer } from "socket.io";
