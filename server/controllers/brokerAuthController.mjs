2d0ea06c3adf2f45505070b48eed63ff6d71632a
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-04 08:24:21 -0900

    working on the broker auth controller

--- server/controllers/brokerAuthController.mjs
+++ server/controllers/brokerAuthController.mjs
@@ -1,3 +1,18 @@
 import Broker from "../models/brokerList.mjs";
 import { hashPassword, comparePassword } from "../helpers/auth.mjs";
 import jwt from "jsonwebtoken";
+
+//show the list of Brokers "all brokers"
+export const showAllBrokers = async (req, res) => {
+  Broker.find()
+    .then((response) => {
+      res.json({
+        response,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: "an error occured",
+      });
+    });
+};
--- server/models/brokerList.mjs
+++ server/models/brokerList.mjs
@@ -1,15 +1,24 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const brokerSchema = new Schema({
-  name: String,
-  email: {
-    type: String,
-    unique: true,
+const brokerSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    PhoneNumber: {
+      type: Number,
+    },
+    password: {
+      type: String,
+    },
   },
-  PhoneNumber: Number,
-  password: String,
-});
+  { timestamps: true }
+);
 
 const Broker = mongoose.model("Broker", brokerSchema);
 
