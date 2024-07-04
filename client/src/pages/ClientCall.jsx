964c722a75928027628a73a72d763e9dc6e2756c
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-03 23:08:51 -0900

    add a peer package and working on the clientcall page

--- client/src/pages/ClientCall.jsx
+++ client/src/pages/ClientCall.jsx
@@ -1,48 +1,68 @@
+// src/ClientCall.jsx
 import React, { useEffect, useRef, useState } from "react";
-// import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
-// import { TextField } from "@material-ui/core";
-// import AssignmentIcon from "@material-ui/icons/Assignment";
-// import PhoneIcon from "@material-ui/icons/Phone";
-// import { CopyToClipboard } from "react-copy-to-clipboard";
-// import peer from "simple-peer";
-// import io from "socket.io-client";
-// import "../assets/style/clientCall.css";
+import io from "socket.io-client";
+import Peer from "peerjs";
 
-// const socket = io.connect("http://localhost:8081");
+const socket = io("http://localhost:5000");
 
-export default function ClientCall() {
+const ClientCall = () => {
+  const [myId, setMyId] = useState("");
   const myVideo = useRef();
   const userVideo = useRef();
   const peerInstance = useRef();
-  const callerVideo = useRef();
-  const localVideo = useRef();
+
+  useEffect(() => {
+    const peer = new Peer(undefined, {
+      host: "/",
+      port: "3001",
+    });
+    peerInstance.current = peer;
+
+    peer.on("open", (id) => {
+      setMyId(id);
+      socket.emit("join-room", "room-id", id);
+    });
+
+    peer.on("call", (call) => {
+      call.answer(myVideo.current.srcObject);
+      call.on("stream", (userVideoStream) => {
+        userVideo.current.srcObject = userVideoStream;
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId);
+    });
+
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        myVideo.current.srcObject = stream;
+        myVideo.current.play();
+      });
+
+    return () => {
+      socket.disconnect();
+      peer.disconnect();
+    };
+  }, []);
+
+  const connectToNewUser = (userId) => {
+    const call = peerInstance.current.call(userId, myVideo.current.srcObject);
+    call.on("stream", (userVideoStream) => {
+      userVideo.current.srcObject = userVideoStream;
+    });
+  };
 
   return (
-    <>
-      <div className="mainContainer">
-        <div className="videoContainer">
-          <div className="callerStream">
-            <video
-              autoPlay
-              playsInline
-              muted
-              ref={callerVideo}
-              className="callerVideo"
-            />
-            <div className="localStream">
-              <video
-                autoPlay
-                playsInline
-                muted
-                ref={localVideo}
-                className="localVideo"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="buttonContainer"></div>
-      </div>
-    </>
+    <div className="App">
+      <video ref={myVideo} muted />
+      <video ref={userVideo} />
+    </div>
   );
-}
+};
+
+export default ClientCall;
