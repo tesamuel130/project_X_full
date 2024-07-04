4f0605f90f851c6151419d489c4ad433d5c6dabf
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-03 23:39:06 -0900

    workig on the video call page

--- server/index.mjs
+++ server/index.mjs
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8081;
 const server = http.createServer(app);
 const io = new SocketIoServer(server, {
   cors: {
-    origin: "http://localhost:8081",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
