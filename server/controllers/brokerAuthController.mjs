199a9e4e27a08d405c77ec63c2749c232e696cf3
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-04 20:43:40 -0900

    working on the broker auth controller

--- server/controllers/brokerAuthController.mjs
+++ server/controllers/brokerAuthController.mjs
@@ -1,10 +1,11 @@
 import Broker from "../models/brokerList.mjs";
 import { hashPassword, comparePassword } from "../helpers/auth.mjs";
 import jwt from "jsonwebtoken";
+import { response } from "express";
 
 //show the list of Brokers "all brokers"
 export const showAllBrokers = async (req, res) => {
-  Broker.find()
+  await Broker.find()
     .then((response) => {
       res.json({
         response,
@@ -16,3 +17,21 @@ export const showAllBrokers = async (req, res) => {
       });
     });
 };
+
+//show a broker by id
+export const showBrokerById = async (req, res) => {
+  const { id } = req.params;
+  await Broker.findById({ id })
+    .then((response) => {
+      res.json({
+        response,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: "an error occured",
+      });
+    });
+};
+
+//
