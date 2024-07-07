ed680136038ab8086072ceb7334108e0f5ca9d65
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-07 00:37:14 -0900

    working on the seller controller

--- server/controllers/sellerControllers.mjs
+++ server/controllers/sellerControllers.mjs
@@ -1,3 +1,35 @@
 import Seller from "../models/seller.mjs";
 
 //filter the seller by service type
+//filter the public chat service type seller
+export const publicChatServiceSeller = async (req, res) => {
+  const serviceType = "PublicChat";
+  try {
+    const chatSellers = await Seller.find({ serviceType: serviceType });
+    res.status(200).json(chatSellers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+//filter the private chat service type seller
+export const privateChatServiceSeller = async (req, res) => {
+  const serviceType = "PrivateChat";
+  try {
+    const chatSellers = await Seller.find({ serviceType: serviceType });
+    res.status(200).json(chatSellers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+//filter the contact in person service type seller
+export const contctInPersonServiceSeller = async (req, res) => {
+  const serviceType = "ContactInPerson";
+  try {
+    const chatSellers = await Seller.find({ serviceType: serviceType });
+    res.status(200).json(chatSellers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
