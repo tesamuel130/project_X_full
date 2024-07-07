36380cf01801530e41834b07eb2b0e6eec837ca7
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-07 07:40:46 -0900

    working on the payment auth page

--- client/src/pages/PaymentAuth.jsx
+++ client/src/pages/PaymentAuth.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 //import fontawsome
