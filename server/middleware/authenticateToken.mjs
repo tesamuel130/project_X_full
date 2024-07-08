cfb4cad2fc9ef4f95d657f9e36bc231c57f2b835
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-07 21:34:21 -0900

    working on the authenticatetoken

---
+++ server/middleware/authenticateToken.mjs
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.sendStatus(401); // Unauthorized if no token provided
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // Forbidden if token is invalid
+    }
+    req.user = user;
+    next(); // Proceed to next middleware or route handler
+  });
+};
