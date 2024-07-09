e575ad308896a3221d63c647c5a5f08ac21b315b
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-08 23:10:47 -0900

    working on the payment controller and autonticatio token

--- server/controllers/paymentControllers.mjs
+++ server/controllers/paymentControllers.mjs
@@ -1,1 +1,17 @@
 import User from "../models/user.mjs";
+
+export const paymentStatusSender = async (req, res) => {
+  try {
+    const { bankUserName, bankAccountNo, currency, amount, paymentMethod } =
+      req.body;
+
+    const recietImage = req.files.map((file) => ({
+      filename: file.filename,
+      path: file.path,
+      mimetype: file.mimetype,
+    }));
+  } catch (error) {
+    console.error("Error saving file:", error);
+    res.status(500).json({ error: "Error saving file" });
+  }
+};
--- server/middleware/authenticateToken.mjs
+++ server/middleware/authenticateToken.mjs
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.mjs";
 
 const JWT_SECRET = process.env.JWT_SECRET; // Replace with your actual secret
 
@@ -10,12 +11,27 @@ const authenticateToken = (req, res, next) => {
     return res.sendStatus(401); // Unauthorized if no token provided
   }
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
+  jwt.verify(token, JWT_SECRET, async (err, user) => {
     if (err) {
       return res.sendStatus(403); // Forbidden if token is invalid
     }
-    req.user = user;
-    next(); // Proceed to next middleware or route handler
+    try {
+      const foundUser = await User.findById(user.id);
+      if (!foundUser) {
+        return res.sendStatus(404); // User not found
+      }
+
+      req.user = {
+        id: foundUser._id,
+        name: foundUser.name,
+        email: foundUser.email,
+      };
+
+      next();
+    } catch (error) {
+      console.error("Error fetching user:", err);
+      return res.sendStatus(500);
+    }
   });
 };
 
