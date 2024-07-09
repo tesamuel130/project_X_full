1090af57ddc5438b50a8e24f965e5d5c3e126021
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-08 23:26:53 -0900

    working on the payment controller

--- server/controllers/paymentControllers.mjs
+++ server/controllers/paymentControllers.mjs
@@ -2,14 +2,26 @@ import User from "../models/user.mjs";
 
 export const paymentStatusSender = async (req, res) => {
   try {
-    const { bankUserName, bankAccountNo, currency, amount, paymentMethod } =
-      req.body;
+    const { transactions } = req.body;
 
     const recietImage = req.files.map((file) => ({
       filename: file.filename,
       path: file.path,
       mimetype: file.mimetype,
     }));
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    user.transaction.push(...JSON.parse(transactions));
+    user.recietImage.push(...recietImage);
+
+    await user.save();
+
+    res.status(201).json(user);
   } catch (error) {
     console.error("Error saving file:", error);
     res.status(500).json({ error: "Error saving file" });
