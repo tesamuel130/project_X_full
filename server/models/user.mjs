e40a978e358f157c995e92b400039deb6e30739f
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-08 22:28:29 -0900

    working on the user schema

--- server/models/user.mjs
+++ server/models/user.mjs
@@ -1,46 +1,49 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: String,
-  email: {
-    type: String,
-    unique: true,
+const userSchema = new Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    transaction: [
+      {
+        accountUserName: {
+          type: String,
+        },
+        accountNumber: {
+          type: String,
+        },
+        currency: {
+          type: String,
+        },
+        amount: {
+          type: Number,
+        },
+        paymentMethod: {
+          type: String,
+        },
+      },
+    ],
+    recietImage: [
+      {
+        filename: {
+          type: String,
+        },
+        path: {
+          type: String,
+        },
+        mimetype: {
+          type: String,
+        },
+      },
+    ],
   },
-  password: String,
-  transaction: [
-    {
-      accountUserName: {
-        type: String,
-      },
-      accountNumber: {
-        type: String,
-      },
-      currency: {
-        type: String,
-      },
-      amount: {
-        type: Number,
-      },
-      paymentMethod: {
-        type: String,
-      },
-    },
-  ],
-  recietImage: [
-    {
-      filename: {
-        type: String,
-      },
-      path: {
-        type: String,
-      },
-      mimetype: {
-        type: String,
-      },
-    },
-  ],
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
