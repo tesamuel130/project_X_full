e24e15d67093e3183e9c063cddd2f2d3851f37df
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-07-15 00:21:38 -0900

    working on the authcontroller

--- server/controllers/authControllers.mjs
+++ server/controllers/authControllers.mjs
@@ -63,9 +63,9 @@ export const loginUser = async (req, res) => {
         (err, token) => {
           if (err) throw err;
           res.cookie("token", token).json(user);
-          res.json({
-            message: "logged in successfully"
-          })
+          return res.json({
+            message: "logged in successfully",
+          });
         }
       );
     }
